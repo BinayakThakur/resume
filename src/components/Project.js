cc450a8f586867fd995389268c0143d08db72821
Binayak Thakur <thakurganesh919@gmail.com>
2021-10-01 02:01:23 +0530

    ..

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,23 +10,23 @@ import Travel from "./Travel";
 const Project =()=>{
     return (<>
 <div className="row">
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <ImgMediaCard></ImgMediaCard>
         </div>
         
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <DFS></DFS>
         </div>
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <Calci></Calci>
         </div>
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <Travel></Travel>
         </div>
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <Spring></Spring>
         </div>
-        <div className="col-3 mt-5">
+        <div className="col mt-5">
         <Express></Express>
         </div>
         </div>
